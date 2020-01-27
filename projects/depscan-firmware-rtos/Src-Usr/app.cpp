17c820bf4167aad0558e0bf889b031296654fc6a
Stephen Kang <ki6080@gmail.com>
2020-01-27 17:55:18 +0900

    Comment outline for init process

--- projects/depscan-firmware-rtos/Src-Usr/app.cpp
+++ projects/depscan-firmware-rtos/Src-Usr/app.cpp
@@ -1,15 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////
 //! Defines fundamental logics for overall system.
 //!
 //! @author Seungwoo Kang (ki6080@gmail.com)
 //! @copyright Copyright (c) 2019. Seungwoo Kang. All rights reserved.
 //!
 //! @details
+#include "rw.h"
 #include <FreeRTOS.h>
 #include <cmsis_os2.h>
 #include <list>
 #include <stdio.h>
-#include <vector> 
-
+#include <vector>
 
 /////////////////////////////////////////////////////////////////////////////
 // Memory allocation rebinds
@@ -36,11 +37,17 @@ void operator delete( void* p )
 }
 
 /////////////////////////////////////////////////////////////////////////////
-// Initialize process
+// Static decls
+static void InitCmdProc();
+
+/////////////////////////////////////////////////////////////////////////////
+// Defines initialize process
 //
-
-extern "C" void InitProcedure( void )
+// - Launches host communication process
+// - Launches command queue/procedure process
+// - Initializes hi-precision hardware timer
+// - Initializes motor control logic
+extern "C" void InitProcedure()
 {
-
+    InitRW();
 }
-
