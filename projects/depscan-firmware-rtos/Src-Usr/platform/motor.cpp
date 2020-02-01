4934dca17c3a58fd1d77a0efcec1877a1507689f
Stephen Kang <ki6080@gmail.com>
2020-02-01 19:22:08 +0900

    Update .gitignore

---
+++ projects/depscan-firmware-rtos/Src-Usr/platform/motor.cpp
@@ -0,0 +1,10 @@
+//! @file motor.c
+//! @brief Stepper motor implementation
+//!
+//! @author Seungwoo Kang (ki6080@gmail.com)
+//! @copyright Copyright (c) 2019. Seungwoo Kang. All rights reserved.
+//!
+//! @details
+#include "../app/motor.h"
+
+//! @todo Implement this motor implementation
