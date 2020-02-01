4aa4251d198608f4c4ccd7a8d98eb3159c499227
Stephen Kang <ki6080@gmail.com>
2020-02-01 15:48:50 +0900

    Create distance sensor impl file

---
+++ projects/depscan-firmware-rtos/Src-Usr/platform/argus/dist-sensor.cpp
@@ -0,0 +1,33 @@
+#include "..\..\app\dist-sensor.h"
+
+//! Static sensor instance
+static struct dist_sens__ {
+
+} s_inst;
+
+//! Exported distance sensor handle
+dist_sens_t ghDistSens = &s_inst;
+
+extern "C" {
+void DistSens_Configure( dist_sens_t, dist_sens_config_t const* opt )
+{
+}
+
+void DistSens_GetConfigure( dist_sens_t, dist_sens_config_t* out )
+{
+}
+
+void DistSens_GetDistanceFxp( dist_sens_t, fxp9_22_t* out )
+{
+}
+
+bool DistSens_MeasureSync( dist_sens_t, uint32_t Retry )
+{
+    return false;
+}
+
+bool DistSens_MeasureAsync( dist_sens_t, uint32_t Retry, void* cb_obj, dist_sens_async_cb_t cb )
+{
+    return false;
+}
+}
